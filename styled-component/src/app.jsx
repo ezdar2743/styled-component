5a9c96ac2c72deea7474972d397730350aee5c64
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-10 15:46:46 +0900

    style extend and copy

--- styled-component/src/app.jsx
+++ styled-component/src/app.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Father = styled.div`
+display: flex;
+`;
+const Box = styled.div`
+background-color: ${(props) => props.bgColor};
+width: 100px;
+height: 100px;
+`;
+
+const Circle = styled(Box)`
+border-radius: 50%;
+`;
 
 const App = () => {
   return (
-    <div>
-      ss
-    </div>
+    <Father>
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
+    </Father>
   );
 };
 
